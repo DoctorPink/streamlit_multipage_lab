e3fe3cf8625b9cb4c4580aa30c8b9d10b69c0bca
DoctorPink <65646938+DoctorPink@users.noreply.github.com>
2025-07-31 11:14:10 -0400

    Update 3_📊_DataFrame_Demo.py

--- pages/3_📊_DataFrame_Demo.py
+++ pages/3_📊_DataFrame_Demo.py
@@ -5,8 +5,8 @@ from urllib.error import URLError
 
 st.set_page_config(page_title="DataFrame Lab", page_icon="📊")
 
-st.markdown("# DataFrame Demo")
-st.sidebar.header("DataFrame Demo")
+st.markdown("# DataFrame Lab")
+st.sidebar.header("DataFrame Lab")
 st.write(
     """This demo shows how to use `st.write` to visualize Pandas DataFrames.
 (Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
